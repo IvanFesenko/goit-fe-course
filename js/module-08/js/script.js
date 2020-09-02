fc23b87d66e46d31889123869f77555ef3367013
Ivan Fesenko <pharmivan@gmail.com>
2020-09-02 16:30:03 +0300

    module 8 some life added

--- js/module-08/js/script.js
+++ js/module-08/js/script.js
@@ -1,46 +1,97 @@
 import gallery from "./gallery-items.js";
-const galleryList = document.querySelector(".gallery");
-const btnClose = document.querySelector(".lightbox__button");
-const lightbox = document.querySelector(".lightbox");
-const openedImg = document.querySelector(".lightbox__image");
 
-function createGalleryItem(image) {
-  const galleryItem = document.createElement("li");
+const refs = {
+  galleryList: document.querySelector(".gallery"),
+  btnClose: document.querySelector('button[data-action="close-lightbox"]'),
+  lightbox: document.querySelector(".lightbox"),
+  openedImg: document.querySelector(".lightbox__image"),
+  imgSources: [],
+};
+
+function createGalleryItem(image, index) {
+  const galleryItem = createElementWithAttributes("li");
   galleryItem.classList.add("gallery__item");
-
-  const galleryLink = document.createElement("a");
+  const galleryLink = createElementWithAttributes("a", {
+    href: image.original,
+  });
   galleryLink.classList.add("gallery__link");
-  galleryLink.setAttribute("href", image.original);
-
-  const galleryImage = document.createElement("img");
+  const galleryImage = createElementWithAttributes("img", {
+    src: image.preview,
+    "data-source": image.original,
+    alt: image.description,
+  });
   galleryImage.classList.add("gallery__image");
-  galleryImage.setAttribute("src", image.preview);
-  galleryImage.setAttribute("data-source", image.original);
-  galleryImage.setAttribute("alt", image.description);
+  refs.imgSources.push({ id: index, src: image.original });
 
   galleryLink.appendChild(galleryImage);
   galleryItem.appendChild(galleryLink);
-
   return galleryItem;
+}
+
+function createElementWithAttributes(name, attributes = {}) {
+  const Element = document.createElement(name);
+  for (const attribute in attributes) {
+    Element.setAttribute(attribute, attributes[attribute]);
+  }
+  return Element;
 }
 
 function createGallery() {
   const layout = gallery.map(createGalleryItem);
-  galleryList.append(...layout);
+  refs.galleryList.append(...layout);
 }
 
 function onGalleryClick(event) {
-  console.log("test");
   event.preventDefault();
-  lightbox.classList.add("is-open");
-  openedImg.setAttribute("src", event.target.dataset.source);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  refs.lightbox.classList.add("is-open");
+  changeModalSrc(event.target.dataset.source);
+  window.addEventListener("keydown", onPressKey);
+}
+
+function changeModalSrc(src) {
+  refs.openedImg.setAttribute("src", src);
+}
+
+function getModalSrc() {
+  return refs.openedImg.getAttribute("src");
 }
 
 function closeModal() {
-  lightbox.classList.remove("is-open");
+  refs.lightbox.classList.remove("is-open");
+  changeModalSrc("");
+}
+
+function onPressKey(event) {
+  if (event.code === "Escape") {
+    closeModal();
+  } else if (event.code === "ArrowLeft") {
+    let current = refs.imgSources.findIndex((el) => el.src === getModalSrc());
+    if (current === 0) {
+      current = refs.imgSources.length;
+    }
+    const newIndex = refs.imgSources.find((el, i) => i === current - 1);
+    changeModalSrc(newIndex.src);
+  } else if (event.code === "ArrowRight") {
+    let current = refs.imgSources.findIndex((el) => el.src === getModalSrc());
+    if (current === refs.imgSources.length - 1) {
+      current = -1;
+    }
+    const newIndex = refs.imgSources.find((el, i) => i === current + 1);
+    changeModalSrc(newIndex.src);
+  }
+}
+
+function onOverlayClick(event) {
+  if (event.target !== refs.openedImg) {
+    closeModal();
+  }
 }
 
 createGallery();
 
-galleryList.addEventListener("click", onGalleryClick);
-btnClose.addEventListener("click", closeModal);
+refs.galleryList.addEventListener("click", onGalleryClick);
+refs.btnClose.addEventListener("click", closeModal);
+refs.lightbox.addEventListener("click", onOverlayClick);
